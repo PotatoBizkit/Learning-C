f7da93e360e3aa025dc3b274a52f1f82a2157dee
Chetan <chetanx777@gmail.com>
2025-03-19 15:33:13 +0530

    Rectangle area with hard inputs

---
+++ 03_Area_Of_A_Rectangle.c
@@ -0,0 +1,9 @@
+//Area of rectangle using hard coded inputs
+#include <stdio.h>
+
+int main()
+{
+    int length = 6, width = 9;
+    printf("The area of the Rectangle with length 6m and width 9m is %dm^2.",length*width);
+    return 0;
+}
