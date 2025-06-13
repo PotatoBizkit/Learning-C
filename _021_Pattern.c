ce626be673c2adca10d17c6388880ee924ac9f8d
Chetan <chetanx777@gmail.com>
2025-06-13 19:13:09 +0530

    Program to print lower triangular pattern

---
+++ _021_Pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void Lower_Triangle(int x, char c){
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int rows;
+    char sym;
+    printf("Enter the number of rows you want:\n");
+    scanf("%d", &rows);
+    while (getchar() != '\n');
+    printf("Enter the character symbol:\n");
+    scanf("%c", &sym);
+    while (getchar() != '\n');
+    printf("\n");
+    Lower_Triangle(rows, sym);
+    return 0;
+}
