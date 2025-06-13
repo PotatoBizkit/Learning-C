fed086c07c1d11cfab6499b2dce37775551f0ee4
Chetan <chetanx777@gmail.com>
2025-06-13 20:28:34 +0530

    Program to print inverted triangular pattern

---
+++ _022_Pattern.c
@@ -0,0 +1,33 @@
+// To print pattern:
+// ...
+// ****
+// ***
+// **
+// *
+
+#include <stdio.h>
+
+void Inverted_Triangle(int x, char c){
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = x; j >= i; j--)
+        {
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int rows;
+    char sym;
+    printf("Enter the number of rows you want:\n");
+    scanf("%d", &rows);
+    while (getchar() != '\n');
+    printf("Enter the character symbol:\n");
+    scanf("%c", &sym);
+    while (getchar() != '\n');
+    printf("\n");
+    Inverted_Triangle(rows, sym);
+    return 0;
+}
