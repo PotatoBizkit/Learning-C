ed0665a0224ff30ada3d0e4260b75c6d03320163
Chetan <chetanx777@gmail.com>
2025-06-13 16:01:50 +0530

    Program to calculate fibonacci using recursion

---
+++ _020_Fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int Fib(int x){
+    if (x == 1 || x == 2)
+    {
+        return 1;
+    }
+    
+    return Fib(x-1) + Fib(x-2);
+}
+
+int main(){
+    int num, fib;
+    printf("Enter the element number of fibonacci sequence you want:\n");
+    scanf("%d", &num);
+    if (num < 0)
+    {
+        printf("Fibonacci cannot be performed for negative integers");
+        return 1;
+    }
+    fib = Fib(num);
+    printf("The element number %d of Fibonacci sequence is %d", num, fib);
+
+    return 0;
+}
