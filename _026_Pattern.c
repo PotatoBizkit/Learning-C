b625a50264432e3950efb0f4aa3f3aa6aa214865
Chetan <chetanx777@gmail.com>
2025-06-14 17:08:13 +0530

    Program to print inverted pyramid pattern

---
+++ _026_Pattern.c
@@ -0,0 +1,37 @@
+// To print pattern:
+// ...
+// *******
+//  *****
+//   ***
+//    *
+
+#include <stdio.h>
+
+void Inverted_Triangle(int x, char c){
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2*(x-i)+1); k++)
+        {
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int rows;
+    char sym;
+    printf("Enter the number of rows you want:\n");
+    scanf("%d", &rows);
+    while (getchar() != '\n');
+    printf("Enter the character symbol:\n");
+    scanf("%c", &sym);
+    while (getchar() != '\n');
+    printf("\n");
+    Inverted_Triangle(rows, sym);
+    return 0;
+}
