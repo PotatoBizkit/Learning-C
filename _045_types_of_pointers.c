ebb994021caf41e021ac3c1d06f4a03ae0302dfa
Chetan <chetanx777@gmail.com>
2025-06-28 16:40:25 +0530

    program to  understand types of pointers

---
+++ _045_types_of_pointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x = 69;
+    float y = 4.20;
+
+    // Void pointer (General purpose pointer)
+    // void* ptr;
+    // ptr = &x;
+    // printf("%d", *((int*)ptr));
+
+    // NULL ((void*)0) pointer
+    // int* ptr = NULL; // Do not dreference here
+
+    // Dangling pointer (points to freed memory location or location whose content has been deleted)
+    
+    // Wild pointer (Uninitialized pointer)
+
+    return 0;
+}
